3476734a5c14f7b2edbbf1a7f9a2bdd17c1cee00
Milan2509 <milan@milanverstappen.nl>
2024-11-19 11:24:45 +0100

    database aanmaak

--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -15,7 +15,13 @@ with sqlite3.connect("MCPizzeria.db") as db:
 
 
 ### ---------  Functie definities  -----------------
-
+def maakTabellenAan():
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbl_pizzas(
+        gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+        gerechtNaame TEXT NOT NULL,
+        gerechtPrijs REAL NOT NULL);""")
+    print("Tabel aangemaakt 'tbl_pizzas' aangemaakt")
 
 ### --------- Hoofdprogramma  ---------------
-
+maakTabellenAan()
